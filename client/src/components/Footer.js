59bdb0aca2e5bed03be1f6b2d8599c7d68b792d6
Emilykradke <emilykradke@gmail.com>
2019-07-25 14:48:28 -0500

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,93 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import API from '../utils/API';
+import Scrollspy from 'react-scrollspy';
 
 class Footer extends Component {
+
+  state = {
+    hours: [],
+    contact: {}
+  }
+
+  componentDidMount() {
+    this.loadHours();
+    this.loadContact();
+  }
+
+  loadHours = () => {
+    API.getHours() 
+    .then(res =>
+      this.setState({
+        hours: res.data
+      }, () => {console.log(this.state.hours)})
+    )
+    .catch(err => console.log(err))
+  }
+
+  loadContact = () => {
+    API.getContact() 
+    .then(res =>
+      this.setState({
+        contact: res.data
+      }, () => {console.log(this.state.contact)})
+    )
+    .catch(err => console.log(err))
+  }
+
   render() {
+    const { hours, contact } = this.state;
+
     return (
-      <div>
-        <p onClick={this.props.click}>Login Modal</p>
-      </div>
+      <Fragment>
+        <div className='link-container'>
+          <div className='quicklinks'>
+            <h2>Quick Links</h2>
+            <Scrollspy className='nav-list'
+                items={ ['home', 'about', 'contact', 'services'] } offset={ -1000 }>
+                  <li>
+                    <a href={'#home'}>Home</a>
+                    <a href={'#services'}>Services</a>
+                    <a href={'#about'}>About</a>
+                    <a href={'#contact'}>Contact</a>
+                  </li>
+              </Scrollspy>
+              <p onClick={this.props.click}>Login Modal</p>
+          </div>
+
+          <div className='hours-container'>
+            
+            <h2>Hours</h2>
+            <div className='hours'>
+              {hours.map(hour => (
+                <p className='hour' key={hour._id}>
+                    {hour.day}: {hour.hours}
+                </p>
+              ))}
+            </div>
+
+          </div>
+
+          <div className='contact-container'>
+
+            <h2>How to Find Me</h2>
+            <div className='contact'>
+                <div className='address'>
+                  <p className='street'>
+                    {contact.address}
+                  </p>
+                </div>    
+            </div>
+
+          </div>
+        </div>
+
+        <div className='copyright-container'>
+          <div className='copyright'>
+            © 2019 Hair by Julie G
+          </div>
+        </div>
+      </Fragment>
+      
     );
   }
 }
