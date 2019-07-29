b72f155ad45a1535109a50a78bdaa8d43e8530ca
Emilykradke <emilykradke@gmail.com>
2019-07-29 13:37:04 -0500

    appointment modal

--- client/src/components/AppointmentModal.js
+++ client/src/components/AppointmentModal.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AppointmentModal = props => (
-    <div className='loginModal'>
-        <p className='modalX' onClick={props.click}>x</p>
-    </div>
-)
+class AppointmentModal extends Component {
+  render() {
+    return (
+      <div className='appointment-modal'>
+        <p className='modalX' onClick={this.props.click}>x</p>
+        <form>
+          <fieldset className='appointment-container'>
+            <h2 className='title'>
+              Request an appointment for your hair transformation with Julie G
+            </h2>
+            <p className='instructions'>
+              Please use the following from to request an appointment time that is convenient. Your preferred time for your appointent will be confirmed by phone. 
+            </p>
+            <p className='walkins'>
+              Please note that Julie G does not accept walk-in appointments
+            </p>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AppointmentModal;
