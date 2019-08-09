ef9112741732eb90d49c3bde5ec956e66759affc
Emilykradke <emilykradke@gmail.com>
2019-08-09 14:34:27 -0500

    seed file

--- server.js
+++ server.js
@@ -55,7 +55,8 @@ db.on("error", (error) => {
 
 // Logs success if sucessfully connected to db
 db.once("open", () => {
-	console.log("DB connection successful!");
+  console.log("DB connection successful!");
+  require('./scripts/seedDB');
 });
 
 /*****************|
